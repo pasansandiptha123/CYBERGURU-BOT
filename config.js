4fba3c882782a2ad2ae20abc2b80768c50058cd7
pasansandiptha123 <126784869+pasansandiptha123@users.noreply.github.com>
2023-08-31 20:15:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94716500044', 'me', true],
+  ['94716500044', 'guru', true], 
   [''] 
 ] //Number of owners
 
